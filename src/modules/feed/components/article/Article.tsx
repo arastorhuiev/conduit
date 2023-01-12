8486b51326816a64e073b3ed5ebd83c54b28186f
Andrii Rastorhuiev <arastorhuiev@gmail.com>
2023-01-12 23:49:35 +0200

    fix path in Link

--- src/modules/feed/components/article/Article.tsx
+++ src/modules/feed/components/article/Article.tsx
@@ -25,7 +25,7 @@ export const Article: FC<ArticleProps> = ({
       <div className='border-t border-black/10 py-6'>
         <div className='mb-4 font-light flex justify-between'>
           <div className='flex'>
-            <Link to={`/@${author.username}`}>
+            <Link to={`/${author.username}`}>
               <img
                 src={author.image}
                 alt={`${author.username} avatar`}
@@ -33,7 +33,7 @@ export const Article: FC<ArticleProps> = ({
               />
             </Link>
             <div className='mr-6 ml-0.3 leading-4 inline-flex flex-col'>
-              <Link to={`/@${author.username}`} className='font-medium'>
+              <Link to={`/${author.username}`} className='font-medium'>
                 {author.username}
               </Link>
               <span className='text-conduit-gray-500 text-date'>
